a3e4569a996dcff9dcc4130cf06d4e57364a52c1
mark <sunduo3195@qq.com>
2020-01-31 15:39:22 +0800

    Updates

---
+++ precache-manifest.05ea05c2182c770f440961a47c922dd6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ccfd7463b577e18b38bd6be9c596a4ec",
+    "url": "/2019-ncov/index.html"
+  },
+  {
+    "revision": "cf42b1a69bf3da773477",
+    "url": "/2019-ncov/static/css/main.6fbb1773.chunk.css"
+  },
+  {
+    "revision": "8f2ad18975cd18ac172b",
+    "url": "/2019-ncov/static/js/2.97f848b4.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/2019-ncov/static/js/2.97f848b4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "cf42b1a69bf3da773477",
+    "url": "/2019-ncov/static/js/main.13c775b1.chunk.js"
+  },
+  {
+    "revision": "9ec61bf1fff76c065256",
+    "url": "/2019-ncov/static/js/runtime-main.2907b2a1.js"
+  },
+  {
+    "revision": "dd8c6eebf343adf689501b5896b4d483",
+    "url": "/2019-ncov/static/media/feiyan.dd8c6eeb.png"
+  }
+]);
